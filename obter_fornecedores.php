1e6fa4a04317c0cf15ca13402623c2be3f7e54f0
alcantarajv <joaovitoralcantaracorrea@gmail.com>
2024-03-28 00:42:29 -0300

    melhoria nos códigos

--- obter_fornecedores.php
+++ obter_fornecedores.php
@@ -1,13 +1,10 @@
 <?php
-// Inclua o arquivo de conexão com o banco de dados
 require_once 'conexao.php';
 
-// Consulte o banco de dados para obter os dados dos fornecedores
 $query = "SELECT * FROM fornecedores";
 $stmt = $db->query($query);
 $fornecedores = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Retorne os dados dos fornecedores em formato JSON
 header('Content-Type: application/json');
 echo json_encode($fornecedores);
 ?>
--- obter_produtos.php
+++ obter_produtos.php
@@ -1,16 +1,12 @@
 <?php
-// Incluir o arquivo de conexão com o banco de dados
 require_once 'conexao.php';
 
-// Consulta SQL para obter os dados dos produtos juntamente com o nome e o ID do fornecedor
 $query = "SELECT produtos.id, produtos.nome, produtos.preco, fornecedores.id AS fornecedor_id, fornecedores.nome AS nome_fornecedor
           FROM produtos
           INNER JOIN fornecedores ON produtos.fornecedor_id = fornecedores.id";
 
-// Preparar e executar a consulta
 $stmt = $db->query($query);
 $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Retornar os dados dos produtos em formato JSON
 echo json_encode($produtos);
 ?>
